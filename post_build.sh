fc15645b56509d59b9270ad16259003e917bcbd8
Petter Rasmussen <petter@hask.no>
2022-08-28 15:46:28 +0300

    Update post build script

--- post_build.sh
+++ post_build.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 set -e
 
-cargo run -p sevenguis_cli -- home_page > dist/index.html
-cargo run -p sevenguis_cli -- counter_page > dist/counter.html
-cargo run -p sevenguis_cli -- temperature_page > dist/temperature.html
-cargo run -p sevenguis_cli -- timer_page > dist/timer.html
-cargo run -p sevenguis_cli -- crud_page > dist/crud.html
-cargo run -p sevenguis_cli -- flight_page > dist/flight.html
+
+#!/bin/bash
+set -e
+
+env="$1"
+event="$2"
+target="debug"
+
+
+if [[ "$env" == "release" ]]; then
+    target="release"
+fi
+
+
+if [[ "$event" == "after_asset_hash" || "$env" == "dev" ]]; then
+    # Generate html
+    ./target/$target/sevenguis_cli home_page > dist/index.html
+    ./target/$target/sevenguis_cli counter_page > dist/counter.html
+    ./target/$target/sevenguis_cli temperature_page > dist/temperature.html
+    ./target/$target/sevenguis_cli timer_page > dist/timer.html
+    ./target/$target/sevenguis_cli crud_page > dist/crud.html
+    ./target/$target/sevenguis_cli flight_page > dist/flight.html
+fi
+
